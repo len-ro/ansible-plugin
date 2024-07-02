58734b4c34545375856e8c31e26609a33c5df242
Marilen Corciovei <marilen.corciovei@mydiapason.com>
2024-07-02 15:04:40 +0200

    patch for V4.0.1, rundeck 5.4.0

--- src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerContextBuilder.java
+++ src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerContextBuilder.java
@@ -879,4 +879,15 @@ public class AnsibleRunnerContextBuilder {
         }
         return options;
     }
+
+    public Map<String,String> getJobOptions(){
+        Map<String, String> options = new HashMap<>();
+
+        //also append all `job` env variables
+        Map<String, String> jobOptions = context.getDataContext().get("job");
+        for (Map.Entry<String, String> entry : jobOptions.entrySet()) {
+            options.put("RD_JOB_" + entry.getKey().toUpperCase(), entry.getValue());
+        }
+        return options;
+    }
 }
